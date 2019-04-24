5154d359ddf1d22be7d386fbe875813739d94ee2
Daniel <garciaantoniodaniel11@gmail.com>
2019-04-24 11:59:28 +0200

    Wpf_slideBar lo más importante Binding y el menú superior

---
+++ WPF_Slider/WPF_Slider/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Slider
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Inicializar();
+        }
+
+        void Inicializar()
+        {
+            recPanel.Opacity = sldOpacidad.Value;
+        }
+
+        private void sldOpacidad_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            recPanel.Opacity = sldOpacidad.Value;
+            tbxValor.Text = (sldOpacidad.Value * 100).ToString("000") + "%";
+        }
+    }
+}
